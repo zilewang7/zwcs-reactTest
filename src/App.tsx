b2f377449e6f2060e5c47d8d6adf2e4180f96a9a
DDC <zilewang7@gmail.com>
2022-04-26 11:59:45 +0800

    hacker

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import zwcs from './mockdata/zwcs.json'
 import Zwcs from './components/zwcs'
 
-
+// const hacker = "<h1>Hacked!</h1>";
+const jsHacker = "javascript: alert('Hacked!');"
 
 function App() {
   return (
-    <ul>
-      {zwcs.map( z => <Zwcs id = {z.id} name = {z.name} email = {z.email}/>)}
-    </ul>
+    <>
+      {/* {hacker} */}
+      <a href={jsHacker}>hacker</a>
+      <ul>
+        {jsHacker}
+        {zwcs.map( z => <Zwcs id = {z.id} name = {z.name} email = {z.email}/>)}
+      </ul>
+    </>
   );
 }
 
